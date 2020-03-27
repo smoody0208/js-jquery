24392ad5203d40268355edb19b24493a4d1ff87b
Spencer Moody <spencer.moody@outlook.com>
2020-03-27 12:31:46 -0700

    Add else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function) {
-  $("form#quiz").submit(function(event) {
+$(document).ready(function(){
+  $("form#quiz").submit(function(event){
     var nameInput = $("input#name").val();
     var devInput = $("input#development").val();
     var fieldInput = $("input#field").val();
@@ -7,12 +7,24 @@ $(document).ready(function) {
     var platInput = $("input#platform").val();
     var easyInput = $("input#easy").val();
 
+    event.preventDefault();
+
     $(".name").text(nameInput);
 
     if (devInput === "front-end") {
-      $("#a-js").show():
-      else if (fieldInput === "web" && comInput === "startup" && platInput === "apple")
+      $("#a-js").show();
+    } 
+    else if (fieldInput === "web" && comInput === "startup" && platInput === "apple" && easyInput === "no") {
+      $("#a-ruby").show();
+    } 
+    else if (fieldInput === "game" && comInput === "established" && platInput === "apple" & easyInput === "no") {
+      $("#a-ruby").show();
     }
-
-  }
-}
+    else if (fieldInput === "mobile" && comInput === "large" && platInput === "windows" && easyInput === "no") {
+      $("#a-csharp").show(); 
+    }
+    else {  
+    (fieldInput === "game" && comInput === "established" && platInput === "apple" && easyInput === "no") 
+    }
+  });
+});
