33b3551550bfcf5d9740eccf4638c28c7831e3e4
Spencer Moody <spencer.moody@outlook.com>
2020-03-27 13:14:12 -0700

    fixed else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
-$(document).ready(function(){
-  $("form#quiz").submit(function(event){
-    var nameInput = $("input#name").val();
-    var devInput = $("input#development").val();
-    var fieldInput = $("input#field").val();
-    var comInput = $("input#company").val();
-    var platInput = $("input#platform").val();
-    var easyInput = $("input#easy").val();
-
-    event.preventDefault();
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    var nameInput = $("#name").val();
+    var devInput = $("#development").val();
+    var fieldInput = $("#field").val();
+    var comInput = $("#company").val();
+    var platInput = $("#platform").val();
+    var easyInput = $("#easy").val();
 
     $(".name").text(nameInput);
 
     if (devInput === "front-end") {
       $("#a-js").show();
-    } 
-    else if (fieldInput === "web" && comInput === "startup" && platInput === "apple" && easyInput === "no") {
+    } else if (fieldInput === "web" && comInput === "startup" && platInput === "apple" && easyInput === "no") {
       $("#a-ruby").show();
-    } 
-    else if (fieldInput === "game" && comInput === "established" && platInput === "apple" & easyInput === "no") {
+      $("#a-js").hide();
+    } else if (fieldInput === "web" && comInput === "established" && platInput === "apple" & easyInput === "no") {
       $("#a-ruby").show();
+      $("#a-js").hide();
+    } else if (fieldInput === "web" && comInput === "large" && platInput === "windows" && easyInput === "no") {
+      $("#a-csharp").show();
+      $("#a-js").hide(); 
+    } else if (fieldInput === "game" && comInput === "large" && platInput === "windows" && easyInput === "no") {
+      $("#a-csharp").show();
+      $("#a-js").hide(); 
+    } else {
+      $("#nope").show();
     }
-    else if (fieldInput === "mobile" && comInput === "large" && platInput === "windows" && easyInput === "no") {
-      $("#a-csharp").show(); 
-    }
-    else {  
-    (fieldInput === "game" && comInput === "established" && platInput === "apple" && easyInput === "no") 
-    }
+    event.preventDefault();
   });
 });
