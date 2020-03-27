23de266c1a0e9ec06218614a73fd2677b9aed880
Spencer Moody <spencer.moody@outlook.com>
2020-03-27 16:33:36 -0700

    fixed issues with hiding input

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,31 @@ $(document).ready(function() {
 
     if (devInput === "front-end") {
       $("#a-js").show();
+      $("#a-ruby").hide();
+      $("#nope").hide();
+      $("a-csharp").hide()
     } else if (fieldInput === "web" && comInput === "startup" && platInput === "apple" && easyInput === "no") {
-      $("#a-ruby").show().siblings().hide("#a-ruby");
+      $("#a-ruby").show();
+      $("#a-js").hide();
+      $("#nope").hide();
+      $("a-csharp").hide();
     } else if (fieldInput === "web" && comInput === "established" && platInput === "apple" & easyInput === "no") {
-      $("#a-ruby").show().siblings().hide("#a-ruby");
+      $("#a-ruby").show();
+      $("#a-js").hide();
+      $("#nope").hide();
+      $("a-csharp").hide()
     } else if (fieldInput === "web" && comInput === "large" && platInput === "windows" && easyInput === "no") {
-      $("#a-csharp").show().siblings().hide("#a-csharp");
+      $("#a-csharp").show();
+      $("#a-js").hide();
+      $("#nope").hide();
+      $("a-ruby").hide()
     } else if (fieldInput === "game" && comInput === "large" && platInput === "windows" && easyInput === "no") {
       $("#a-csharp").show().siblings().hide("#a-csharp");
     } else {
-      $("#nope").show().sibilings().hide("#nope");
+      $("#nope").show();
+      $("#a-js").hide();
+      $("#a-ruby").hide();
+      $("a-csharp").hide()
     }
     event.preventDefault();
   });
